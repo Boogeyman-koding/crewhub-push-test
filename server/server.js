daeb62ae3aa2c247084c3e9d4bd1c6c4e0a79c3d
proto <prototypzero82@gmail.com>
2026-07-04 03:08:15 +0300

    Fix Vercel deploy

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const webpush = require("web-push");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-    res.send("CrewHub server работает 🍻");
+    res.send("CrewHub server работает через Vercel 🍻");
 });
 
-app.listen(3000, () => {
-    console.log("CrewHub server запущен на порту 3000");
-});
+
+module.exports = app;
